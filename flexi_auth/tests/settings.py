b0eae98d4871504651c30290a1797744e16dadb6
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-09-12 12:40:09 +0200

    Added db-related settings to the settings file used for unit tests.

--- flexi_auth/tests/settings.py
+++ flexi_auth/tests/settings.py
@@ -1,6 +1,36 @@
+import os
+DIRNAME = os.path.dirname(__file__)
+
+DEFAULT_CHARSET = 'utf-8'
+
+test_engine = os.environ.get("FLEXI_AUTH_TEST_ENGINE", "django.db.backends.sqlite3")
+
+DATABASES = {
+    'default': {
+        'ENGINE': test_engine,
+        'NAME': os.environ.get("FLEXI_AUTH_DATABASE_NAME", "flexi_auth_test"),
+        'USER': os.environ.get("FLEXI_AUTH_DATABASE_USER", ""),
+        'PASSWORD': os.environ.get("FLEXI_AUTH_DATABASE_PASSWORD", ""),
+        'HOST': os.environ.get("FLEXI_AUTH_DATABASE_HOST", "localhost"),
+    }
+}
+
+if test_engine == "django.db.backends.sqlite3":
+    DATABASES['default']['NAME'] = os.path.join(DIRNAME, 'flexi_auth_test.db')
+    DATABASES['default']['HOST'] = ""
+elif test_engine == "django.db.backends.mysql":
+    DATABASES['default']['PORT'] = os.environ.get("FLEXI_AUTH_DATABASE_PORT", 3306)
+elif test_engine == "django.db.backends.postgresql_psycopg2":
+    DATABASES['default']['PORT'] = os.environ.get("FLEXI_AUTH_DATABASE_PORT", 5432)
+
 INSTALLED_APPS = (
     'flexi_auth',
     'flexi_auth.tests',
      # other dependencies go here 
 )
 
+ROLES_LIST = ()
+
+PARAM_CHOICES = ()
+
+VALID_PARAMS_FOR_ROLES = {}
