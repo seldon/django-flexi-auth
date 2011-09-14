43d8691c32b11fe0c081b0254b484f69ff2d42b4
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-09-14 16:53:56 +0200

    Minor refactoring.
    * Renamed ``@permission_required()`` decorator to ``@object_permission_required()``
      in order to avoid confusion with Django's built-in one;
    * Tweaked decorator's docstring.

--- flexi_auth/decorators.py
+++ flexi_auth/decorators.py
@@ -3,14 +3,16 @@ from django.contrib.auth.decorators import user_passes_test
 
 from flexi_auth.models import ObjectWithContext
 
-def permission_required(perm, obj, login_url=None, raise_exception=False, **context):
+def object_permission_required(perm, obj, login_url=None, raise_exception=False, **context):
     """
     Decorator for views that checks whether a user has been granted the permission ``perm``
-    on the object ``obj`` within the context ``context``.
+    on the object ``obj`` with respect to the context ``context``.
+    
     If the permission check fails, user will be redirected to the log-in page 
     (as specified by the ``login_url`` parameter) or, if the ``raise_exception`` parameter 
-    is ``True``, the ``PermissionDenied`` exception will be raised instead.
+    is set to ``True``, the ``PermissionDenied`` exception will be raised instead.
     """
+     
     def check_perms(user):
         # First check if the user has the permission (even anonymous users)
         model_or_instance = obj
